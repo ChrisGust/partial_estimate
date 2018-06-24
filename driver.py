8be910cf170eaf47f72bc0c6ef3b579ccec93882
Chris Gust <chrisgust@me.com>
2018-06-24 16:36:44 -0400

    Adds plot of decision rule

--- driver.py
+++ driver.py
@@ -7,7 +7,7 @@ import sys
 importlib.reload(pa)
 
 #Read in nonlinear model and parameter values
-params = {'beta' : 0.99,'delta':0.025,'alpha': 0.3,'a': 4.6,'b' : 0.05,'gamma_A' : 0.00,'gamma_V': 0.00,'rhomu': 0.7,'stdmu' : 0.006,
+params = {'beta' : 0.99,'delta':0.025,'alpha': 0.3,'a': 4.6,'b' : 0.05,'gamma_A' : 0.00,'gamma_V': 0.00,'rhomu': 0.7,'stdmu' : 0.0075,
              'rhobeta' : 0.5, 'stdbeta': 0.0035}
 phiitilde = params['b']*params['a']**2
 #params['a'] = -20.0
@@ -79,8 +79,8 @@ else:
     ncheb = 3*np.ones(nmsv-2,dtype=int)
     msvmax = np.zeros(nmsv-2)
 ngrid = np.ones(ninnov,dtype=int)
-msvmax[0] = 0.1
-msvmax[1] = 0.2
+msvmax[0] = 0.05
+msvmax[1] = 0.15
 maxstd = 2.0
 ngrid[0] = 7
 if ninnov > 1:
@@ -106,9 +106,9 @@ invcoeff_lin = pd.DataFrame(acoeff0[:,0:poly1['npoly']],columns=['cons','invm1',
 #outputswitch = 0, do nothing
 #outputswitch = 1, get irfs and plot
 #outputswitch = 2, simulate data
+#outputswitch = 3, plot decision rule for investment
 ###########################################################################################################################3
-outputswitch = 0
-
+outputswitch = 2
 if outputswitch == 0:
     print('Investment Decision rule Coeffs')
     print(invcoeff_nl.round(3))
@@ -121,22 +121,24 @@ elif outputswitch == 1:
     for x in varlist:
         endogvarm1[x] = paramplus[x]
         endogvarm1[x+'_d'] = 0.0
-    endogvarm1['beta_d'] = 0.02
-    endogvarm1['mu_d'] = 0.02
+    #endogvarm1['beta_d'] = 0.02
+    endogvarm1['mu_d'] = -0.02
+    endogvarm1['kp_d'] = -0.05
     endogvarm1_b = endogvarm1.copy()
     innov = np.zeros([poly1['ne']])
-    innov[0] = 2.0
+    innov[0] = -2.0
     df1 = pa.simulate(TT,endogvarm1,endogvarm1_b,innov,paramplus,acoeff0,poly0,varlist,irfswitch)
     df2 = pa.simulate(TT,endogvarm1,endogvarm1_b,innov,paramplus,acoeff1,poly1,varlist,irfswitch)
 
     #run shocks in opposite direction
     endogvarm1['beta_d'] = -endogvarm1['beta_d']
     endogvarm1['mu_d'] = -endogvarm1['mu_d']
+    endogvarm1['kp_d'] = endogvarm1['kp_d']
     innov[0] = -innov[0]
     if poly0['ne'] > 1:
         innov[1] = -innov[1]
     df3 = pa.simulate(TT,endogvarm1,endogvarm1_b,innov,paramplus,acoeff1,poly1,varlist,irfswitch)
-    df3 = -df3
+    df3 = df3
 
     #Plot IRFs of linear and nonlinear models
     fig2, axs2 = plt.subplots(2, 2, tight_layout=True)
@@ -166,11 +168,11 @@ elif outputswitch == 1:
             axs3[i-2,1].set_title(varplot[i])   
     axs3[1,1].legend()
     plt.show()
-else:
+elif outputswitch == 2:
     irfswitch = 0
     TT = 5000
     varlist = ['kp','inv','qq','mu','beta','dinv','invrate']
-    endogvarm1 = {x: 0.0 for x in varlist} 
+    endogvarm1 = {x: 0.0 for x in varlist}
     for x in varlist:
         endogvarm1[x] = paramplus[x]
         endogvarm1[x+'_d'] = 0.0
@@ -178,7 +180,53 @@ else:
     innov = np.zeros([poly1['ne']])
     df1 = pa.simulate(TT,endogvarm1,endogvarm1_b,innov,paramplus,acoeff0,poly0,varlist,irfswitch)
     df2 = pa.simulate(TT,endogvarm1,endogvarm1_b,innov,paramplus,acoeff1,poly1,varlist,irfswitch)
-    df2stats = df2.describe()
+    df2stats = df2.describe()   
+else:
+    varlist = ['kp','inv','qq','mu','beta','dinv','invrate']
+    endogvarm1 = {x: 0.0 for x in varlist} 
+    for x in varlist:
+        endogvarm1[x] = paramplus[x]
+        endogvarm1[x+'_d'] = 0.0
+    innov = np.zeros([poly1['ne']])
+    
+    
+    Nx = 50
+    Ns = 3
+    mu_lb = -0.5*msvmax[0]
+    xx = np.linspace(mu_lb,-mu_lb,Nx)
+    inv_nl = np.zeros([Ns,Nx])
+    inv_lin = np.zeros([Ns,Nx])
+
+    shockval = np.zeros(Ns)
+    shockval[0] = (poly1['exoggrid'][0,0]+0.001)/paramplus['rhomu']
+    #shockval[0] = -0.5*msvmax[1]
+    shockval[Ns-1] = -shockval[0]
+
+    for j in np.arange(Ns):
+        for i in np.arange(Nx):
+            endogvarm1['mu_d'] = shockval[j]
+            #endogvarm1['inv_d'] = shockval[j]
+            endogvarm1['kp_d'] = xx[i]
+            endogvar = pa.decr(endogvarm1,innov,paramplus,acoeff1,poly1)
+            endogvar_lin = pa.decr(endogvarm1,innov,paramplus,acoeff0,poly0)
+            inv_nl[j,i] = 100.0*endogvar['inv_d']
+            inv_lin[j,i] = 100.0*endogvar_lin['inv_d']
+
+    #plot investment decision rule 
+    fig2, axs2 = plt.subplots(1,1)
+    axs2.plot(100*xx,inv_lin[1,:],'k-',linewidth=3)
+    axs2.plot(100*xx,inv_nl[1,:],'r:',linewidth=3,label='Nonlinear, ss mu')
+    axs2.plot(100*xx,inv_lin[0,:],'k-',linewidth=3)
+    axs2.plot(100*xx,inv_nl[0,:],'r:',linewidth=3,label='Nonlinear, low mu')
+    axs2.plot(100*xx,inv_lin[2,:],'k-',linewidth=3)
+    axs2.plot(100*xx,inv_nl[2,:],'r:',linewidth=3,label='Nonlinear, high mu')
+    axs2.set_title('Investment as a function of Lagged K')
+    axs2.set_xlabel('Kp(-1)')
+    axs2.set_ylabel('Investment')
+    axs2.legend()
+    plt.show()
+
+    
 
 
 
